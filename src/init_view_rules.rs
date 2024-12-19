009cf14558689de7273d2e57ba57074aac0b370f
yes7rose <yes7rose@sina.com>
2024-12-19 12:53:54 +0800

    重构视图规则初始化

--- src/init_view_rules.rs
+++ src/init_view_rules.rs
@@ -1,25 +1,25 @@
-use dependencies_sync::bson::{self, doc};
+use dependencies_sync::bson::{self, doc, Document};
+use dependencies_sync::log;
+use dependencies_sync::mongodb::IndexModel;
+use dependencies_sync::rust_i18n::{self, t};
 use dependencies_sync::toml::map::Map;
 use dependencies_sync::toml::Value;
-use dependencies_sync::log;
 
+use entity::get_new_entity_id;
+use manage_define::cashmere::ViewRuleLevel;
 use manage_define::field_ids::*;
 use manage_define::general_field_ids::*;
 use manage_define::manage_ids::*;
 
 use define_utils as utils;
 
-pub async fn init_view_rules(
-    tomls: &Vec<Map<String, Value>>,
-    root_id: &str,
-    root_group_id: &str,
-) {
+pub async fn init_view_rules(tomls: &Vec<Map<String, Value>>, root_id: &str, root_group_id: &str) {
     for map in tomls {
-        let rule_id = utils::get_id(map).unwrap();
-        let rule_name = match utils::get_name_map(map) {
+        let manage_id = utils::get_id(map).unwrap();
+        let manage_name = match utils::get_name_map(map) {
             Some(m) => m,
             None => {
-                log::error!("取得映像规则名失败: {} ", rule_id);
+                log::error!("取得映像规则名失败: {} ", manage_id);
                 continue;
             }
         };
@@ -29,25 +29,240 @@ pub async fn init_view_rules(
             None => continue,
         };
 
-        let mut rulse_doc = doc! {
-            "_id": rule_id.to_string(),
-            ID_FIELD_ID.to_string(): rule_id.to_string(),
-            NAME_MAP_FIELD_ID.to_string(): rule_name.clone(),
-            VIEW_RULES_MANAGE_FIELD_ID.to_string(): bson::to_bson(&view_rules.manage).unwrap(),
-            VIEW_RULES_COLLECTION_FIELD_ID.to_string(): bson::to_bson(&view_rules.collection).unwrap(),
-            VIEW_RULES_ENTITY_FIELD_ID.to_string(): bson::to_bson(&view_rules.schema).unwrap(),
-        };
+        let mut docs: Vec<Document> = vec![];
+        // 管理级
+        for (group, rule) in view_rules.manage {
+            let mut rule_doc = Document::new();
+            rule_doc.insert(
+                VIEW_RULES_LEVEL_FIELD_ID.to_string(),
+                ViewRuleLevel::VlManage as i32,
+            );
+            rule_doc.insert(
+                VIEW_RULES_SUBJECT_MANAGE_FIELD_ID.to_string(),
+                manage_id.clone(),
+            );
+            rule_doc.insert(VIEW_RULES_SUBJECT_FIELD_ID.to_string(), manage_id.clone());
+            rule_doc.insert(VIEW_RULES_GROUP_FIELD_ID.to_string(), group);
+            rule_doc.insert(
+                VIEW_RULES_READ_RULE_FIELD_ID.to_string(),
+                rule.read_rule as i32,
+                // bson::to_bson(&rule.read_rule).unwrap(),
+            );
+            rule_doc.insert(
+                VIEW_RULES_READ_FILTERS_FIELD_ID.to_string(),
+                bson::to_bson(
+                    &rule
+                        .read_filters
+                        .iter()
+                        .map(|x| *x as i32)
+                        .collect::<Vec<i32>>(),
+                )
+                .unwrap(),
+            );
+            rule_doc.insert(
+                VIEW_RULES_WRITE_RULE_FIELD_ID.to_string(),
+                // bson::to_bson(&rule.write_rule).unwrap(),
+                rule.write_rule as i32,
+            );
+            rule_doc.insert(
+                VIEW_RULES_WRITE_FILTERS_FIELD_ID.to_string(),
+                bson::to_bson(
+                    &rule
+                        .write_filters
+                        .iter()
+                        .map(|x| *x as i32)
+                        .collect::<Vec<i32>>(),
+                )
+                .unwrap(),
+            );
+            docs.push(rule_doc);
+        }
 
-        match entity::insert_entity(
-            VIEW_RULES_MANAGE_ID,
-            &mut rulse_doc,
-            root_id,
-            root_group_id,
-        )
-        .await
-        {
-            Ok(_r) => continue,
-            Err(e) => println!("{} {}", e.operation(), e.details()),
+        // 集合级
+        for (group, rule) in view_rules.collection {
+            let mut rule_doc = Document::new();
+            rule_doc.insert(
+                VIEW_RULES_LEVEL_FIELD_ID.to_string(),
+                ViewRuleLevel::VlCollection as i32,
+            );
+            rule_doc.insert(
+                VIEW_RULES_SUBJECT_MANAGE_FIELD_ID.to_string(),
+                manage_id.clone(),
+            );
+            rule_doc.insert(VIEW_RULES_SUBJECT_FIELD_ID.to_string(), manage_id.clone());
+            rule_doc.insert(VIEW_RULES_GROUP_FIELD_ID.to_string(), group);
+            rule_doc.insert(
+                VIEW_RULES_READ_RULE_FIELD_ID.to_string(),
+                rule.read_rule as i32,
+                // bson::to_bson(&rule.read_rule).unwrap(),
+            );
+            rule_doc.insert(
+                VIEW_RULES_READ_FILTERS_FIELD_ID.to_string(),
+                bson::to_bson(
+                    &rule
+                        .read_filters
+                        .iter()
+                        .map(|x| *x as i32)
+                        .collect::<Vec<i32>>(),
+                )
+                .unwrap(),
+            );
+            rule_doc.insert(
+                VIEW_RULES_WRITE_RULE_FIELD_ID.to_string(),
+                // bson::to_bson(&rule.write_rule).unwrap(),
+                rule.write_rule as i32,
+            );
+            rule_doc.insert(
+                VIEW_RULES_WRITE_FILTERS_FIELD_ID.to_string(),
+                bson::to_bson(
+                    &rule
+                        .write_filters
+                        .iter()
+                        .map(|x| *x as i32)
+                        .collect::<Vec<i32>>(),
+                )
+                .unwrap(),
+            );
+            docs.push(rule_doc);
+        }
+
+        // 实体集
+        for (group, rule) in view_rules.entity {
+            let mut rule_doc = Document::new();
+            rule_doc.insert(
+                VIEW_RULES_LEVEL_FIELD_ID.to_string(),
+                ViewRuleLevel::VlEntity as i32,
+            );
+            rule_doc.insert(
+                VIEW_RULES_SUBJECT_MANAGE_FIELD_ID.to_string(),
+                manage_id.clone(),
+            );
+            rule_doc.insert(VIEW_RULES_SUBJECT_FIELD_ID.to_string(), manage_id.clone());
+            rule_doc.insert(VIEW_RULES_GROUP_FIELD_ID.to_string(), group);
+            rule_doc.insert(
+                VIEW_RULES_READ_RULE_FIELD_ID.to_string(),
+                rule.read_rule as i32,
+                // bson::to_bson(&rule.read_rule).unwrap(),
+            );
+            rule_doc.insert(
+                VIEW_RULES_READ_FILTERS_FIELD_ID.to_string(),
+                bson::to_bson(
+                    &rule
+                        .read_filters
+                        .iter()
+                        .map(|x| *x as i32)
+                        .collect::<Vec<i32>>(),
+                )
+                .unwrap(),
+            );
+            rule_doc.insert(
+                VIEW_RULES_WRITE_RULE_FIELD_ID.to_string(),
+                // bson::to_bson(&rule.write_rule).unwrap(),
+                rule.write_rule as i32,
+            );
+            rule_doc.insert(
+                VIEW_RULES_WRITE_FILTERS_FIELD_ID.to_string(),
+                bson::to_bson(
+                    &rule
+                        .write_filters
+                        .iter()
+                        .map(|x| *x as i32)
+                        .collect::<Vec<i32>>(),
+                )
+                .unwrap(),
+            );
+            docs.push(rule_doc);
+        }
+
+        // 字段级
+        for (field, rules) in view_rules.fields {
+            for (group, rule) in rules {
+                let mut rule_doc = Document::new();
+                rule_doc.insert(
+                    VIEW_RULES_LEVEL_FIELD_ID.to_string(),
+                    ViewRuleLevel::VlField as i32,
+                );
+                rule_doc.insert(
+                    VIEW_RULES_SUBJECT_MANAGE_FIELD_ID.to_string(),
+                    manage_id.clone(),
+                );
+                rule_doc.insert(VIEW_RULES_SUBJECT_FIELD_ID.to_string(), field.clone());
+                rule_doc.insert(VIEW_RULES_GROUP_FIELD_ID.to_string(), group);
+                rule_doc.insert(
+                    VIEW_RULES_READ_RULE_FIELD_ID.to_string(),
+                    rule.read_rule as i32,
+                    // bson::to_bson(&rule.read_rule).unwrap(),
+                );
+                rule_doc.insert(
+                    VIEW_RULES_READ_FILTERS_FIELD_ID.to_string(),
+                    bson::to_bson(
+                        &rule
+                            .read_filters
+                            .iter()
+                            .map(|x| *x as i32)
+                            .collect::<Vec<i32>>(),
+                    )
+                    .unwrap(),
+                );
+                rule_doc.insert(
+                    VIEW_RULES_WRITE_RULE_FIELD_ID.to_string(),
+                    // bson::to_bson(&rule.write_rule).unwrap(),
+                    rule.write_rule as i32,
+                );
+                rule_doc.insert(
+                    VIEW_RULES_WRITE_FILTERS_FIELD_ID.to_string(),
+                    bson::to_bson(
+                        &rule
+                            .write_filters
+                            .iter()
+                            .map(|x| *x as i32)
+                            .collect::<Vec<i32>>(),
+                    )
+                    .unwrap(),
+                );
+                docs.push(rule_doc);
+            }
+        }
+
+        for mut doc in docs {
+            let id = if let Some(i) = get_new_entity_id(VIEW_RULES_MANAGE_ID, root_id).await {
+                i
+            } else {
+                log::error!("{}: {}", t!("取得新实体ID失败"), VIEW_RULES_MANAGE_ID);
+                continue;
+            };
+            doc.insert(ID_FIELD_ID.to_string(), id.to_string());
+
+            match entity::insert_entity(VIEW_RULES_MANAGE_ID, &mut doc, root_id, root_group_id)
+                .await
+            {
+                Ok(_r) => continue,
+                Err(e) => println!("{} {}", e.operation(), e.details()),
+            }
         }
     }
+    
+    // 初始化视图规则索引, VIEW_RULES_LEVEL_FIELD_ID, VIEW_RULES_SUBJECT_MANAGE_FIELD_ID, VIEW_RULES_SUBJECT_FIELD_ID, VIEW_RULES_GROUP_FIELD_ID
+    if let Some(c) = database::get_collection_by_id(VIEW_RULES_MANAGE_ID).await {
+        // 复合索引顺序影响查询效率
+        let model = IndexModel::builder().keys(doc! {
+            VIEW_RULES_GROUP_FIELD_ID.to_string(): 1,
+            // VIEW_RULES_SUBJECT_MANAGE_FIELD_ID.to_string(): 1,
+            // VIEW_RULES_LEVEL_FIELD_ID.to_string(): 1,
+            // VIEW_RULES_SUBJECT_FIELD_ID.to_string(): 1,
+        }).build();
+
+        match c.create_index(model).await{
+            Ok(_r) => {
+                log::info!("{}: {}", t!("初始化视图规则索引成功"), VIEW_RULES_MANAGE_ID);
+            },
+            Err(e) => {
+                log::error!("{}: {}", t!("初始化视图规则索引失败"), VIEW_RULES_MANAGE_ID);
+                log::error!("{}: {}", t!("错误信息"), e);
+            }
+        };
+    } else {
+        log::error!("{}: {}", t!("获取集合失败"), VIEW_RULES_MANAGE_ID);
+        log::error!("{}: {}", t!("初始化视图规则索引失败"), VIEW_RULES_MANAGE_ID);
+    };
 }
